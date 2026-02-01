80fe688be1b1052e811b62b467ce5838fe4240e8
cloke <cory@me.com>
2026-02-01 09:15:36 -0500

    Initial Commit

---
+++ DockhandTests/DockhandTests.swift
@@ -0,0 +1,16 @@
+//
+//  DockhandTests.swift
+//  DockhandTests
+//
+//  Created by Cory Loken on 2/1/26.
+//
+
+import Testing
+
+struct DockhandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
